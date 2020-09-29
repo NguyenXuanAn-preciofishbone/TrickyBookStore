e47a6127b90234a25866a70d1361978aa76ae7f2
Nguyen Xuan An <lanehacker7294@yahoo.com>
2020-09-29 09:51:13 +0700

    Add project files.

---
+++ TrickyBookStore.Models/PriceDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickyBookStore.Models
+{
+    class PriceDetail
+    {
+        public int Id { get; set; }
+        public int OldBookDiscount { get; set; }
+        public int NewBookDiscount { get; set; }
+
+        public int NewBookDiscountAmmount { get; set; }
+    }
+}
